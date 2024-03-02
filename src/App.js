5982a44fc31ed14bb5f80733542c62e2478706db
Valio Angelov <shindovalio@gmail.com>
2024-03-02 18:29:13 +0200

    edit the app content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Change.
+          Welcome to Angel Solutions!
         </p>
         <a
           className="App-link"
